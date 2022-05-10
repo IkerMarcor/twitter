9aead841ca39c41983250b734be5acb197d753d6
cuateverde <ikermarcor5@gmail.com>
2022-05-10 16:29:48 -0700

    Paso 6: Agregando la clase User con su respectiva prueba

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,5 +1,10 @@
-describe("Preba de jest", () => {
-    test("Prueba 1", () => {
-        expect(10).toBe(9)
+const User = require("./../../app/models/User")
+
+describe("Preba de User", () => {
+    test("Creación de User", () => {
+        const user = new User(1,"IkerMarcor","Iker","Bio","dateCreated","lastUpdate")
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("IkerMarcor")
+
     })
 })
